7ab6b2b15a45769a9fd75e5fcdfc1ec17574d237
Aaron Tam <atam342007@gmail.com>
2025-05-23 13:45:22 -0400

    cleaned files

--- src/facility/facility_types/defense.ts
+++ src/facility/facility_types/defense.ts
@@ -2,10 +2,11 @@ import { Facility, FacilitySector, FacilityType } from "../facility.js";
 
 /** Represents a Defense Facility and its components */
 export class DefenseFacility extends Facility {
-    protected static readonly _FACILITY_SECTOR = FacilitySector.DEFENSE;
-    protected static readonly _FACILITY_TYPE = FacilityType.DEFENSE;
-    protected static readonly _NAME = "Planetary Defense System";
-    protected static readonly _BUILD_COST = 1000000000000;
+    protected static readonly _FACILITY_SECTOR: FacilitySector = FacilitySector.DEFENSE;
+    protected static readonly _FACILITY_TYPE: FacilityType = FacilityType.DEFENSE;
+    protected static readonly _NAME: string = "Planetary Defense System";
+    protected static readonly _BUILD_COST: number = 1_000_000_000_000;
+    protected static readonly _POWER_COST: number = 0;
     protected _maintenanceCost: number = 0;
     protected _pollution: number = 0;
     protected _taxRevenue: number = 0;
